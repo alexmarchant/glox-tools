7debc486b959e546fe93abe23ecf194114ec58ef
Alex Marchant <alexjmarchant@gmail.com>
2019-06-15 22:53:05 -0400

    Typo

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
 		"WhileStmt":      []string{"Condition Expr", "Body Stmt"},
 		"FunctionStmt":   []string{"Name *Token", "Params []*Token", "Body []Stmt"},
 		"ReturnStmt":     []string{"Keyword *Token", "Value Expr"},
-		"ClassStmt":      []string{"Name *Token", "Methods []*FuntionStmt"},
+		"ClassStmt":      []string{"Name *Token", "Methods []*FunctionStmt"},
 	})
 }
 
